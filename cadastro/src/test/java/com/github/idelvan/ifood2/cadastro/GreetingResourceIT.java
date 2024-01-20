feee540762cdc5f5afb8e93ced6ba48610bdc97e
delvan <delvanpj@gmail.com>
2024-01-19 23:29:53 -0300

    corrigindo blibliotecas do projeto inicial

--- cadastro/src/test/java/com/github/idelvan/ifood2/cadastro/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.idelvan.ifood2.cadastro;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- pedido/src/test/java/com/github/idelvan/ifood2/pedido/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.idelvan.ifood2.pedido;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
