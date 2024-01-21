196b3f5d228af0b40fd58a2f70392c850a08dfc9
delvan <delvanpj@gmail.com>
2024-01-21 17:10:23 -0300

    Criando entidades e endpoits REST

---
+++ cadastro/src/main/java/com/github/idelvan/ifood2/cadastro/Localizacao.java
@@ -0,0 +1,20 @@
+package com.github.idelvan.ifood2.cadastro;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "localizacao")
+public class Localizacao{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public Double latitude;
+
+    public Double longitude;
+
+}
---
+++ cadastro/src/main/java/com/github/idelvan/ifood2/cadastro/RestauranteResource.java
@@ -0,0 +1,21 @@
+package com.github.idelvan.ifood2.cadastro;
+
+import java.util.List;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/restaurantes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestauranteResource {
+
+    @GET
+    public List<Restaurante> ListarRestaurante() {
+        return Restaurante.listAll();
+    }
+
+}
